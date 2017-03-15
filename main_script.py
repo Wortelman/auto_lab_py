aaecb32f6d67fc276ede5aaf2e4967d854b184e1
Wortelman <m7661347@utwente.nl>
2017-03-15 17:09:06 +0100

    Update 1, working ENA5061B scripts

--- main_script.py
+++ main_script.py
@@ -5,5 +5,17 @@ Created on Wed Mar 15 10:59:11 2017
 @author: Niek
 """
 import niek_functions as n
-obj1 = n.ENA_connect();
-n.ENA_setup(obj1,'Mutual')
+import matplotlib.pyplot as plt
+obj1 = n.ENA();
+
+object2 = obj1.setup('Mutual')
+n.time.sleep(1)
+[A,f]=obj1.measure()
+
+
+plt.semilogx(f,A[:,:,0])
+
+#
+##plt.pyplot.semilogx(f,A[:)
+#
+#plt.
