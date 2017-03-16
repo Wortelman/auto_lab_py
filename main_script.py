60e85d714779efb14ff86d5dbfaba0b2aff79bdb
Wortelman <m7661347@utwente.nl>
2017-03-16 11:46:22 +0100

    Updated to spyder3 and python 3.6, now writing to touchstone works and files are put into folder measurments.

--- main_script.py
+++ main_script.py
@@ -6,16 +6,14 @@ Created on Wed Mar 15 10:59:11 2017
 """
 import niek_functions as n
 import matplotlib.pyplot as plt
-obj1 = n.ENA();
+obj1 = n.ENA(); # creation of ENA object
 
-object2 = obj1.setup('Mutual')
-n.time.sleep(1)
+#### Only use this part for first setup, since it does a total reset!
+#obj1.setup('Mutual') # setup the standard <-- watch out with this, callibration is deleted!!
+#n.time.sleep(1) # to fast execution of setup and measure gives error
+
 [A,f]=obj1.measure()
+obj1.write_touchtone(A,f,"measurements/naam")
 
 
 plt.semilogx(f,A[:,:,0])
-
-#
-##plt.pyplot.semilogx(f,A[:)
-#
-#plt.
